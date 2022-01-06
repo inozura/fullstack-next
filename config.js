d4adb3e1d37704a92b0a0b5290f731440825b221
inozura <nodhzen@gmail.com>
2022-01-06 08:21:49 +0700

    change server URI

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://jobpost-olive.vercel.app";
+  : "https://jopost.herokuapp.com";
